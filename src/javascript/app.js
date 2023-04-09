c1dfca14cb818fb03b949dcdb45c2e5f538e3616
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-09 11:19:25 +0530

    updated

--- src/javascript/app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.querySelector('.navbar');
-    if (window.pageYOffset > 0) {
-      navbar.classList.add('sticky');
-    } else {
-      navbar.classList.remove('sticky');
-    }
-  });
-  
