94e9657e700407c58f554a5899d1b14237c0f6fc
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-10 12:13:30 +0530

    updated

---
+++ js/chatbot.js
@@ -0,0 +1,17 @@
+// Get the chatbot icon element
+const chatbotIcon = document.querySelector('#chatbot-icon');
+
+// Get the chatbot element
+const chatbot = document.querySelector('#chatbot');
+
+// Add a click event listener to the chatbot icon element
+chatbotIcon.addEventListener('click', () => {
+  // Toggle the chatbot element's visibility
+  chatbot.classList.toggle('d-none');
+});
+
+// Add a click event listener to the close button
+document.querySelector('#chatbot-close').addEventListener('click', () => {
+  // Hide the chatbot element
+  chatbot.classList.add('d-none');
+});
