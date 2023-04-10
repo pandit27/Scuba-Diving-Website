5e507b007157e605d39e0a3f6c600b5b45f897d6
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-10 21:49:39 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -52,7 +52,6 @@
         }
     });
 
-
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -65,31 +64,6 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
-        dots: true,
-        loop: true,
-        center: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
 
     // Vendor carousel
     $('.vendor-carousel').owlCarousel({
