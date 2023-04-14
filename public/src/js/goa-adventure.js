f0b2799690641b67d7731538530aeb00e06f5458
Priyanshu Jha <priyanshujha131@gmail.com>
2023-04-14 12:13:33 +0530

    updated

---
+++ public/src/js/goa-adventure.js
@@ -0,0 +1,8 @@
+/* window loader start */
+window.addEventListener("load", function () {
+    const loader = document.getElementById("loader");
+    setTimeout(() => {
+        loader.classList.add("hidden");
+    }, 600);
+});
+/* window loader end */
